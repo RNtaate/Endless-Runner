6b8ce8640ec6f657e56e9f9fbc5c2fc82deaddc6
RNtaate <royhyde14@gmail.com>
2021-03-06 22:14:12 +0300

    Add main game scene/starter scene

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('File to hold the main game scene');
+import Phaser from 'phaser';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 960,
+  height: 520,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {y: 0},
+      debug: false
+    }
+  }
+}
+
+const game = new Phaser.Game(config);
