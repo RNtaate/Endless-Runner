a83bbdddc6d1566ab7c1ec85bfdc8746ca680200
RNtaate <royhyde14@gmail.com>
2021-03-06 23:21:36 +0300

    Update preload method with progress bar, create method with game logo

--- src/preload.js
+++ src/preload.js
@@ -1,20 +1,75 @@
 import Phaser from 'phaser';
 
 class Preload extends Phaser.Scene {
-   constructor() {
-     super({key: 'Preload'});
-   }
+  constructor() {
+    super({key: 'Preload'});
+    this.width;
+    this.height;
+  }
 
-   preload() {
+  preload() {
+    this.width = this.scale.width;
+    this.height = this.scale.height;
+    let progressBoxWidth = 320;
+    let progressBoxHeight = 50;
 
-   }
+    let progressBar = this.add.graphics();
+    let progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect( (this.width / 2) - (progressBoxWidth / 2), (this.height / 2) - (progressBoxHeight / 2), progressBoxWidth, progressBoxHeight);
 
-   create() {
-     this.add.text(100, 200, "Preload Scene", {
-       font: '30px monospace',
-       fill: "#ffffff"
-     })
-   }
+    let loadingText = this.add.text(this.width / 2, this.height / 2, 'Loading ...', {
+      font: "20px monospace",
+      fill: '#ffffff'
+    }).setOrigin(0.5);
+
+    // The event listeners for loading assets
+
+    let progressBarWidth = progressBoxWidth - 20;
+    let progressBarHeight = progressBoxHeight - 20
+
+    this.load.on('progress',(value) => {
+      progressBar.clear();
+      progressBar.fillStyle(0xf0ad4e, 1);
+      progressBar.fillRect((this.width / 2) - (progressBarWidth / 2), (this.height / 2) - (progressBarHeight / 2), progressBarWidth * value, progressBarHeight);
+    });
+
+    this.load.on('complete', (e) => {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+    })
+
+
+    // Loading of the assets
+    this.load.image('logo', '../assets/gameLogo.png');
+
+    for(let i = 0; i < 200; i ++) {
+      this.load.image('logo' + i, '../assets/gameLogo.png');
+    }
+
+  }
+
+  create(){
+    this.add.image(this.width / 2, this.height/ 2, 'logo');
+
+    this.message = this.add.text( this.scale.width / 2, 20, 'PRESS "ENTER" TO CONTINUE TO MAIN MENU', {
+      font: "20px monospace",
+      fill: "#ffffff"
+    }).setOrigin(0.5);
+
+    this.message.setAlpha(0);
+
+    this.tweens.add({
+      targets: this.message,
+      repeat: -1,
+      duration: 1000,
+      delay: 1000,
+      ease: 'linear',
+      alpha: 1,
+      yoyo: true
+    })
+  }
 }
 
 export default Preload;
