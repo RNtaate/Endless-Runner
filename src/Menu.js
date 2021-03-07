62ba40023046b06860c4c26fef9f17368396eb7c
RNtaate <royhyde14@gmail.com>
2021-03-07 17:29:51 +0300

    Add more menu buttons to menu scene

--- src/Menu.js
+++ src/Menu.js
@@ -15,12 +15,46 @@ class Menu extends Phaser.Scene {
     this.width = this.scale.width;
     this.height = this.scale.height;
 
-    const button = new CustomButton(this, this.width / 2, this.height / 2, 'startGame', 'startGameHover');
-    this.add.existing(button);
+    this.add.image(this.width / 2, this.height / 2, 'sky').setScale(0.5);
 
-    button.setInteractive().on('pointerover', () => {
+    const startButton = new CustomButton(this, this.width / 2, this.height / 2, 'startGame', 'startGameHover');
+    this.add.existing(startButton);
+
+    startButton.setInteractive().on('pointerover', () => {
       console.log('Thunder over the container');
     })
+
+    const optionsButton = new CustomButton(this, this.width / 4 , this.height / 4, "options", 'optionsHover');
+    this.add.existing(optionsButton);
+
+    optionsButton.setInteractive().on('pointerover', () => {
+      console.log('Thunder over the container');
+    })
+
+    const leaderBoardBtn = new CustomButton(this, (this.width * 3) / 4 , this.height / 4, "leaderBoard", 'leaderBoardHover');
+    this.add.existing(leaderBoardBtn);
+
+    leaderBoardBtn.setInteractive().on('pointerover', () => {
+      console.log('Thunder over the container');
+    })
+
+    const instructionsBtn = new CustomButton(this, (this.width * 3) / 4 , (this.height * 3) / 4, "instructions", 'instructionsHover');
+    this.add.existing(instructionsBtn);
+
+    instructionsBtn.setInteractive().on('pointerover', () => {
+      console.log('Thunder over the container');
+    })
+
+    const creditsBtn = new CustomButton(this, (this.width) / 4 , (this.height * 3) / 4, "credits", 'creditsHover');
+    this.add.existing(creditsBtn);
+
+    creditsBtn.setInteractive().on('pointerover', () => {
+      console.log('Thunder over the container');
+    })
+
+
+
+
   }
 }
 
