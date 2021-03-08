b96b85238506896ae440173f3d8aac9fd0a8f46a
RNtaate <royhyde14@gmail.com>
2021-03-08 22:52:39 +0300

    Add and export gameState object

--- src/boot.js
+++ src/boot.js
@@ -1,6 +1,11 @@
 import Phaser from 'phaser';
 import WebFontFile from './support_script/webfontloader';
 
+let gameState = {
+  sceneWidth: 0,
+  sceneHeight: 0,
+}
+
 class Boot extends Phaser.Scene {
   constructor() {
     super({key: 'Boot'});
@@ -11,9 +16,13 @@ class Boot extends Phaser.Scene {
   }
 
   create() {
+
+    gameState.sceneWidth = this.scale.width;
+    gameState.sceneHeight = this.scale.height;
+
     this.scene.stop();
     this.scene.start('Preload');
   }
 }
 
-export default Boot;
+export {Boot, gameState};
