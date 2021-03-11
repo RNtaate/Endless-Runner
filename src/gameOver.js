8304f1a8ce498fcba715024274b8dae13d45f33e
RNtaate <royhyde14@gmail.com>
2021-03-11 23:44:39 +0300

    Add buttons to gameOver scene

--- src/gameOver.js
+++ src/gameOver.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import {gameState} from './boot';
+import CustomButton from './support_script/CustomButton';
 
 class GameOver extends Phaser.Scene {
   constructor() {
@@ -20,6 +21,24 @@ class GameOver extends Phaser.Scene {
     this.setText(gameState.sceneWidth / 2, gameState.sceneHeight / 2, 'Player 1', '60px', '#ffffff', '#0000ff', 0.5 );
 
     this.setText(gameState.sceneWidth / 2, gameState.sceneHeight / 2 +  heightDet, `Your Score: ${gameState.score}`, '60px', '#ffffff', '#0000ff', 0.5 );
+
+    // Scene Buttons
+    const leaderBoardBtn = new CustomButton(this, (gameState.sceneWidth * 3) / 4 , gameState.sceneHeight - 75, "leaderBoard", 'leaderBoardHover');
+    this.add.existing(leaderBoardBtn);
+
+    leaderBoardBtn.setInteractive().on('pointerover', () => {
+      console.log('Thunder over the container');
+    })
+
+    const playAgainBtn = new CustomButton(this, (gameState.sceneWidth) / 4 , gameState.sceneHeight - 75, "playAgain", 'playAgainHover');
+    this.add.existing(playAgainBtn);
+
+    playAgainBtn.setInteractive().on('pointerover', () => {
+      console.log('Thunder over the container');
+    }).on('pointerup', () => {
+      this.scene.stop();
+      this.scene.start('Game');
+    })
   }
 
   setText(x, y, message, fontSize, strokeColor, fillColor, origin = 0){
